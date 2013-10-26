558a3bdc52474d9e43cbfc7be50aeacf6d5ae1ea
leonardmaurice <succar.leo@gmail.com>
2013-10-26 13:35:43 -0400

    from mini

---
+++ app.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+function renderNewPostForm(request, response) {
+	response.writeHead(200, {
+		'Content-type': 'text/plain'
+	});
+	response.end('Hello World Testing in Mini');
+}
+
+var server = http.createServer(function(request, response) {
+	renderNewPostForm(request,response);
+});
+
+server.listen(8000);
+
+console.log('Listening on http://127.0.0.1:8000');
