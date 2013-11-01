71ad8bfbf7c321689e86ce0c1bceefe643d4f614
Leo Succar <succar.leo@gmail.com>
2013-11-01 01:38:59 -0400

    test

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-var http = require('http');
-
-function renderNewPostForm(request, response) {
-	response.writeHead(200, {
-		'Content-type': 'text/plain'
-	});
-	response.end('Hello World Testing in Mini');
-}
-
-var server = http.createServer(function(request, response) {
-	renderNewPostForm(request,response);
-});
-
-server.listen(8000);
-
-console.log('Listening on http://127.0.0.1:8000');
+require('http').createServer(function handleRequest(request,response) {
+   response.writeHead (200, {'content-type': 'text/plain'});
+   response.end ('i love world');
+})
+.listen(8080);
